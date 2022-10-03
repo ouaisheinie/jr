57adadb030fd83daddc745ce75e3dc4f61178f5d
ouaisheinie <346114608@qq.com>
2022-10-03 15:32:39 +0800

    auth模块

--- src/screens/login/index.tsx
+++ src/screens/login/index.tsx
@@ -1,21 +1,8 @@
+import { useAuth } from "context/auth-context";
 import { FormEvent } from "react";
-const apiUrl = process.env.REACT_APP_API_URL;
 
 export const LoginScreen = () => {
-  // 登录函数
-  const login = (param: { username: string; password: string }) => {
-    fetch(`${apiUrl}/login`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(param),
-    }).then(async (res) => {
-      if (res.ok) {
-        console.log(res);
-      }
-    });
-  };
+  const { login, user } = useAuth();
   // 点击登录
   const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
@@ -28,6 +15,7 @@ export const LoginScreen = () => {
 
   return (
     <form onSubmit={handleSubmit}>
+      {user ? <div>登录成功，用户名：{user?.name}</div> : null}
       <div>
         <label htmlFor="username">用户名</label>
         <input type="text" id="username" />
