f00497d2f125af6bbc6891aa7c868a59a2fc9520
unknown <you@example.com>
2022-10-09 01:27:47 +0800

    解决了 react query问题

--- src/context/index.tsx
+++ src/context/index.tsx
@@ -1,6 +1,13 @@
 import { ReactNode } from "react";
 import { AuthProvider } from "context/auth-context";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+const queryClient = new QueryClient();
 
 export const AppProviders = ({ children }: { children: ReactNode }) => {
-  return <AuthProvider>{children}</AuthProvider>;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>{children}</AuthProvider>
+    </QueryClientProvider>
+  );
 };
