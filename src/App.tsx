837a0622bb31e58e14d49d3a571d527afe1c5d78
ouaisheinie <346114608@qq.com>
2022-10-23 16:09:38 +0800

    errorboundary

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,17 @@ import "./App.css";
 import { useAuth } from "context/auth-context";
 import { AuthenticatedApp } from "authenticated-app";
 import { UnAuthenticatedApp } from "unauthenticated-app";
+import { ErrorBoundary } from "components/error-boundary";
+import { FullPageErrorFallback } from "components/lib";
 
 function App() {
   const { user } = useAuth();
 
   return (
     <div className="App">
-      {user ? <AuthenticatedApp /> : <UnAuthenticatedApp />}
+      <ErrorBoundary fallbackRender={FullPageErrorFallback}>
+        {user ? <AuthenticatedApp /> : <UnAuthenticatedApp />}
+      </ErrorBoundary>
     </div>
   );
 }
